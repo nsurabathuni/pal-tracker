5f9800f4e072d3c6e6b74b549b5c13044892207a
Naveen Surabathuni <nsurabathuni@pivotal.io>
2018-12-29 05:28:08 -0600

    Add CF manifest and controller for env variables

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,31 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+
+    Map<String, String> envMap = new HashMap<>();
+
+    public EnvController(@Value("${PORT:NOT SET}") String port,
+                         @Value("${MEMORY_LIMIT:NOT SET}") String memoryLimit,
+                         @Value("${CF_INSTANCE_INDEX:NOT SET}") String cfInstanceIndex,
+                         @Value("${CF_INSTANCE_ADDR:NOT SET}") String cfInstanceAddr) {
+        envMap.clear();
+        envMap.put("PORT", port);
+        envMap.put("MEMORY_LIMIT", memoryLimit);
+        envMap.put("CF_INSTANCE_INDEX", cfInstanceIndex);
+        envMap.put("CF_INSTANCE_ADDR", cfInstanceAddr);
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+        return envMap;
+    }
+}
